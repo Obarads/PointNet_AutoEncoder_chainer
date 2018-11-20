531475b507dceca90ac8319a8cda4b1065229633
Obarads <b1510803@planet.kanazawa-it.ac.jp>
2018-11-20 11:25:25 +0900

    update

--- train.py
+++ train.py
@@ -1,11 +1,19 @@
 import chainer
 import numpy as np
+from use_senser import dab
 
+class A():
+    def __init__(self):
+        self.n = 113
+
+    def ab(self): return dab
+
+    def ac(self):
+        return self.ab()(19)
 
 def main():
-    n = 100
-
-
+    a = A()
+    print(a.ac())
 
 if __name__ == '__main__':
     main()
